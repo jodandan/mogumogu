08a72d6d74ce3c00e28624bb0064f8fff5c31872
jodan <wdd789q@naver.com>
2023-12-04 19:02:52 +0900

    [feat/Admin] 관리자페이지 로그인 구현 완료

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Note from './pages/Note/Note';
 import Mypage from './pages/Mypage/Mypage';
+import AdminLogin from './pages/AdminLogin/AdminLogin';
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Note />} />
         <Route path="/mypage" element={<Mypage />} />
+        <Route path="/adminlogin" element={<AdminLogin />} />
       </Routes>
     </div>
   );
