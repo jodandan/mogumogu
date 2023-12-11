2c1a0cd9ee811f40293a8e1669a0cc135118817c
jodan <wdd789q@naver.com>
2023-12-11 15:00:36 +0900

    [fix/Admin] 쪽지함 팝업 디자인 수정

--- src/components/NotePagination/NoteDetailPage.js
+++ src/components/NotePagination/NoteDetailPage.js
@@ -3,15 +3,25 @@ import React, { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import Header from '../Header/Header';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
 
 import { ReactComponent as BackbuttonIcon } from '../../assets/Backbutton_icon.svg'
 import Plusbutton from '../../assets/Plusbutton.png'
-import checkmark from '../../assets/checkmark.png'
-import dollar from '../../assets/dollar.png'
-import passbook from '../../assets/passbook.png'
 
 import InputComment from './InputComment';
-import TransactionPopup from './TransactionPopup';
+
+import dollar from '../../assets/dollar.png';
+import checkmark from '../../assets/checkmark.png';
+import passbook from '../../assets/passbook.png';
+
 
 
 const NoteDetailPage = ({ notesData }) => {
@@ -28,8 +38,7 @@ const NoteDetailPage = ({ notesData }) => {
         navigate('/note');
     };
 
-
-    //하드코딩 데이터
+    // 하드코딩 데이터
     const comments = [
         { id: 1, sender: '깜장콩', content: 'Hello!', timestamp: '2023-01-01 12:00' },
         { id: 2, sender: '김조단', content: 'Hi there!', timestamp: '2023-01-01 13:00' },
@@ -41,9 +50,8 @@ const NoteDetailPage = ({ notesData }) => {
     };
 
     const handlePopupOptionClick = (option) => {
-
         console.log(`Selected option: ${option}`);
-        //팝업닫기
+        // 팝업 닫기
         setPopupVisibility(false);
     };
 
@@ -63,9 +71,28 @@ const NoteDetailPage = ({ notesData }) => {
                             style={{ width: '55px', height: '50px', cursor: 'pointer' }}
                             onClick={handlePlusButtonClick}
                         />
-                        {isPopupVisible && <TransactionPopup handlePopupOptionClick={handlePopupOptionClick} />}
                     </PlusButtonBox>
                 </TitleBox>
+                <Drawer anchor="top" open={isPopupVisible} onClose={() => setPopupVisibility(false)}>
+
+                    <List>
+                        {['입금 신청', '거래 완료', '계좌 확인'].map((text, index) => (
+                            <ListItem key={text} disablePadding>
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        {index === 0 && <img src={dollar} alt='dollar' style={{ width: '3vw', height: '3vw', padding: '1vw' }} />}
+                                        {index === 1 && <img src={checkmark} alt="Checkmark Icon" style={{ width: '3vw', height: '3vw', padding: '1vw' }}/>}
+                                        {index === 2 && <img src={passbook} alt="Passbook Icon" style={{ width: '3vw', height: '3vw', padding: '1vw' }}/>}
+                                    </ListItemIcon>
+                                    <ListItemText primary={text} />
+                                </ListItemButton>
+                            </ListItem>
+                        ))}
+                    </List>
+                    <List>
+                        <Text>진행 현황 : 거래 승인</Text>
+                    </List>
+                </Drawer>
                 <PostBody>
                     <ul>
                         {comments.map((comment) => (
@@ -134,6 +161,22 @@ const PlusButtonBox = styled.div`
     flex-shrink: 0;
     background-color: white;
 `;
+
+const Text = styled.div`
+    color: #393939;
+    font-family: Inter;
+    font-size: 25px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    height: 20px;
+    flex-shrink: 0;
+    padding: 5px 46px 20px 46px;
+    width: 20vw;
+`;
+
+
+
 
 
 
