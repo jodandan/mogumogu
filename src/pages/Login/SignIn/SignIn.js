332008398423c99cec0dc86ad7410df079a0be8f
연하영 <dusgkdud7@naver.com>
2023-12-14 01:40:08 +0900

    [feat/Post] 게시글 신고하기 api 연동 완료

--- src/pages/Login/SignIn/SignIn.js
+++ src/pages/Login/SignIn/SignIn.js
@@ -8,21 +8,17 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useNavigate } from 'react-router';
-
+import axios from 'axios';
 
 
 const defaultTheme = createTheme();
 
 export default function SignIn() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-  };
 
+  const handleSubmit = async (event) => { 
+    event.preventDefault();
+    
+  };
 
   const navigate = useNavigate();
 
